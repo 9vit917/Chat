3d9538c170c2f53d459cd7afb790e8d9957585fc
kavi <9vit917@gmail.com>
2020-03-07 23:40:30 +0800

    upgrade message component

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -5,27 +5,67 @@ import classNames from "classnames";
 
 import './index.scss';
 
+
+const setStatus = (isReaded) => {
+    return (
+        <div className="message__status">
+            <span className={classNames("message__status__icon", {"readed": isReaded, "not-readed": !isReaded})}></span>
+        </div>
+    )
+}
+
+const setDate = (date) => {
+    return (
+        <div className="message__content__date">
+            { formatDistanceToNow(new Date(date), {addSuffix: true}) }
+        </div>
+    )
+}
+
+const setText = (text) => {
+    return (
+        <p className="message__content__text">
+            { text }
+        </p>
+    )
+}
+
+const setTypingAnimation = () => {
+    return (
+        <ul className="message__content__typing">
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
+
 const Message = ( props ) => (
-    <div>
-        <div className={ classNames('message' , {"message--friend": props.isMe}) }>
+
+        <div className={ classNames('message' , 
+            {"message--friend": props.isMe, 
+            "message--typing": props.isTyping}) 
+            }>
             <div className="message__avatar">
                 <img src={ props.avatar } alt="avatar" />
             </div>
             <div className="message__content">
-                <div className="message__content__wrapper">
-                    <p className="message__content__text">
-                        { props.text }
-                    </p>
-                </div>
-                <div className="message__content__date">
-                    { formatDistanceToNow(new Date(props.date), {addSuffix: true}) }
-                </div>
+                {   (props.text || props.isTyping) && 
+                    (<div className="message__content__wrapper">
+                        { props.text && setText(props.text) }
+                        { props.isTyping && setTypingAnimation() }
+                    </div>)
+                }
+                {
+                    props.attachment &&
+                    <div className="message__content__attachment">
+                        <img src={props.attachment[0]} alt="attachment"/>
+                    </div>
+                }
+                { props.date && setDate(props.date) }
             </div>
-            <div className="message__status">
-                <span className={classNames("message__status__icon", {"readed": props.isReaded, "not-readed": !props.isReaded})}></span>
-            </div>
+            { !props.isMe && setStatus(props.isReaded) }
         </div>
-    </div>
 )
 
 
@@ -33,6 +73,9 @@ Message.propTypes = {
     avatar: PropTypes.string,
     text: PropTypes.string,
     date: PropTypes.string,
+    isMe: PropTypes.bool,
+    isTyping: PropTypes.bool,
+    attachment: PropTypes.array
 };
 
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,13 +10,25 @@ const Home = () => {
             text="azaza raka maka fo fristylo"
             date="Fri Mar 06 2020 18:41:08"
             isMe={false}
-            isReaded={true}/>
+            isReaded={true}
+            isTyping={false}/>
             <Message
             avatar="https://images.alphacoders.com/711/thumb-350-711581.jpg"
             text="azaza raka maka fo fristylo"
             date="Fri Mar 06 2020 18:41:08"
             isMe={true}
-            isReaded={false}/>
+            isReaded={false}
+            isTyping={false}/>
+            <Message
+            avatar="https://filmschoolrejects.com/wp-content/uploads/2017/04/0JRofTsuy93evl_J5.jpg"
+            isMe={false}
+            isReaded={true}
+            isTyping={true}/>
+            <Message
+            avatar="https://images.alphacoders.com/711/thumb-350-711581.jpg"
+            isMe={true}
+            attachment={["https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png"]}
+            isReaded={true}/>
         </div>
     );
 };
